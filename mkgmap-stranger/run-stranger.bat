6cb19228727823d2b234b70d577333aba93cc929
Larry0ua <pekarskiy.valeriy@gmail.com>
2013-01-30 17:18:17 +0200

    update garmin style - better translations

--- mkgmap-stranger/run-stranger.bat
+++ mkgmap-stranger/run-stranger.bat
@@ -7,13 +7,13 @@ echo Splitting...
 java -Xmx1G -jar ..\executable\splitter\splitter.jar ..\UA.osm.pbf --output-dir=_split
 
 echo Creating map with mkgmap
-java -Xmx1300M -jar ..\executable\mkgmap\mkgmap.jar --read-config=ukrstranger.cfg stranger\stranger.typ
+java -Xmx1300M -jar ..\executable\mkgmap\mkgmap.jar --read-config=ukrstranger.cfg stranger\stranger.txt
 
 echo Copying result files
 copy _intermediate\gmapsupp.img ..\results\
 
 echo Removing intermediate files...
-del /Q _intermediate
-del /Q _split
+rem del /Q _intermediate
+rem del /Q _split
 
 cd ..

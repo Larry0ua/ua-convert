65af48f3b4af1e404eefd80101515a444c158c95
Larry0ua <pekarskiy.valeriy@gmail.com>
2013-02-01 12:07:02 +0200

    complete-ways in navitel

--- navitel/generate-navitel.bat
+++ navitel/generate-navitel.bat
@@ -7,7 +7,7 @@ for /F "tokens=1,2,3 delims=," %%A in (..\other\regions.csv) Do (
  rem %%B - relation ID
  rem %%C - UTF-8 region name
  if not exist extracts\%%A.poly ..\executable\getbound.pl --onering --noinner -o extracts\%%A.poly %%B
- if not exist extracts\%%A.osm ..\executable\osmconvert ../UA.osm.pbf -B=extracts\%%A.poly  --complex-ways  -o=extracts\%%A.osm
+ if not exist extracts\%%A.osm ..\executable\osmconvert ../UA.osm.pbf -B=extracts\%%A.poly  --complete-ways --complex-ways  -o=extracts\%%A.osm
  if not exist maps\%%A.mp ..\executable\osm2mp\osm2mp.pl --config=..\executable\osm2mp\cfg-navitel\navitel-uk.cfg  --target-lang=uk --default-lang=uk  --defaultcountry="UA" --defaultregion="UA" -o maps\Navitel-%%A.mp extracts\%%A.osm
 )
 if exist maps ConvertToNM2.vbs

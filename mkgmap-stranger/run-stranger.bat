9f4b9a4fe2996863682a4fb073d2de300e263c1e
Larry0ua <pekarskiy.valeriy@gmail.com>
2013-02-12 11:03:29 +0200

    Update conversion Garmin style to current Stranger version

--- mkgmap-stranger/run-stranger.bat
+++ mkgmap-stranger/run-stranger.bat
@@ -3,7 +3,7 @@ echo Splitting...
 java -Xmx1G -jar ..\executable\splitter\splitter.jar ..\UA.osm.pbf --output-dir=_split
 
 echo Creating map with mkgmap
-java -Xmx1300M -jar ..\executable\mkgmap\mkgmap.jar --read-config=ukrstranger.cfg stranger\stranger.txt
+java -Xmx1300M -jar ..\executable\mkgmap\mkgmap.jar --read-config=ukrstranger.cfg stranger-ru\stranger.typ
 
 echo Copying result files
 copy _intermediate\gmapsupp.img ..\results\

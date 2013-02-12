aa086d03c2e75f1067c1490f9cdedb1a6622189d
Larry0ua <pekarskiy.valeriy@gmail.com>
2013-02-12 13:14:21 +0200

    More changes for -ru and -uk styles (no translation yet)

--- mkgmap-stranger/run-stranger.bat
+++ mkgmap-stranger/run-stranger.bat
@@ -2,8 +2,13 @@
 echo Splitting...
 java -Xmx1G -jar ..\executable\splitter\splitter.jar ..\UA.osm.pbf --output-dir=_split
 
-echo Creating map with mkgmap
-java -Xmx1300M -jar ..\executable\mkgmap\mkgmap.jar --read-config=ukrstranger.cfg stranger-ru\stranger.typ
+echo Creating -ru map with mkgmap
+java -Xmx1300M -jar ..\executable\mkgmap\mkgmap.jar style-file=stranger-ru/stranger --read-config=ukrstranger.cfg stranger-ru\stranger.typ
+
+ren _intermediate\gmapsupp.img _intermediate\gmapsup2.img
+
+echo Creating -uk map with mkgmap
+java -Xmx1300M -jar ..\executable\mkgmap\mkgmap.jar style-file=stranger-uk/stranger --read-config=ukrstranger.cfg stranger-uk\stranger.typ
 
 echo Copying result files
-copy _intermediate\gmapsupp.img ..\results\
+copy _intermediate\gmapsup?.img ..\results\
---
+++ mkgmap-stranger/stranger-uk/readme.txt
@@ -0,0 +1,2 @@
+extracted from the repository: https://velo100mapper.googlecode.com/svn/trunk (and translated)
+by: maxvasilev

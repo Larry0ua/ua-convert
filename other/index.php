9d2bb8af79cd2cd68019882dbf608951f25c8b84
Larry0ua <pekarskiy.valeriy@gmail.com>
2013-02-21 16:46:33 +0200

    update to index.php

--- other/index.php
+++ other/index.php
@@ -1,4 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Конвертації в формат навігаторів OpenStreetMap України</title>
+  <meta name="description" content="Garmin and Navitel conversions OpenStreetMap Ukraine">
+  <meta name="author" content="SitePoint">
+</head>
+<body>
 <?php
+error_reporting(E_ERROR);
 // Garmin formats
 function file_link_n_details($name) {
 	$res = "<a href='$name'>$name</a> (";
@@ -7,23 +17,25 @@ function file_link_n_details($name) {
 	return $res;
 }
 function print_garmin() {
-	$lst = glob("gmap*.zip");
+	$lst = glob("gmap*.img.zip");
 	if (!$lst) { echo "Нема доступних файлів конвертації в формат Garmin<br>"; return; }
-	echo "Вся Україна (туристичний стиль від Max Vasilev&GaM, прокладка маршрута працює, без адресного пошуку)<br>";
-	if (in_array("gmapsupp.zip", $lst))
-		echo "Українська версія " . file_link_n_details("gmapsupp.zip") . "<br>";
-	if (in_array("gmapsup2.zip", $lst))
-		echo "Русская версия " . file_link_n_details("gmapsup2.zip") . "<br>";
+	echo "Вся Україна (туристичний стиль від Max Vasilev, прокладка маршрута працює, без адресного пошуку)<br>\n<table><tr><th>Опис</th><th>Посилання</th></tr>";
+	if (in_array("gmapsupp.img.zip", $lst))
+		echo "<tr><td>Українська версія</td><td>" . file_link_n_details("gmapsupp.img.zip") . "</td></tr>";
+	if (in_array("gmapsup2.img.zip", $lst))
+		echo "<tr><td>Русская версия</td><td>" . file_link_n_details("gmapsup2.img.zip") . "</td></tr>";
+	echo "</table>";
 }
 function print_navitel() {
 	$fp = fopen("regions.csv", "r");
-	echo "Конвертація в формат Navitel NM2<br>";
+	echo "Конвертація в формат Navitel NM2<br><table><tr><th>Регіон</th><th>Посилання</th></tr>";
 	while (($str = fgets($fp, 1000)) !== FALSE) {
 		$data = explode(',', $str);
-		if (file_exists("Navitel-${data[0]}.zip")) {
-			echo $data[2] ." ". file_link_n_details("Navitel-${data[0]}.zip") . "<br>"; 
+		if (file_exists("Navitel-${data[0]}.nm2.zip")) {
+			echo "<tr><td>" . $data[2] ."</td><td>". file_link_n_details("Navitel-${data[0]}.nm2.zip") . "</td></tr>"; 
 		}
 	}
+	echo "</table>";
 	//	if (!$lst) { echo "Нема доступних файлів конвертації в формат Navitel<br>"; return; }
 
 
@@ -33,3 +45,5 @@ print_garmin();
 echo "<br>";
 print_navitel();
 ?>
+</body>
+</html>

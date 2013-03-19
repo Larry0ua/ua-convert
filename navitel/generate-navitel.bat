a9ee4d65af2972607d619023270cb8d0fa331fe8
Larry0ua <pekarskiy.valeriy@gmail.com>
2013-03-19 17:02:49 +0200

    Garmin - update address search (country, region, city filling) and boundary creation

--- navitel/generate-navitel.bat
+++ navitel/generate-navitel.bat
@@ -2,7 +2,7 @@ for /F "tokens=1,2,3 delims=," %%A in (..\other\regions.csv) Do (
  rem %%A - region code
  rem %%B - relation ID
  rem %%C - UTF-8 region name
- if not exist extracts\%%A.poly ..\executable\getbound.pl --onering --noinner --api=op_ru --offset=0.2 -o extracts\%%A.poly %%B
+ if not exist extracts\%%A.poly ..\executable\getbound.pl --onering --noinner --api=op_ru --offset=0.005 -o extracts\%%A.poly %%B
 REM if not exist extracts\exp-%%A.poly ..\executable\offset\offset.exe extracts\%%A.poly extracts\exp-%%A.poly
  if not exist extracts\%%A.osm ..\executable\osmconvert ../UA.osm.pbf -B=extracts\%%A.poly -o=extracts\%%A.osm
  if not exist maps\Navitel-%%A.mp ..\executable\osm2mp\osm2mp.pl --config=..\executable\osm2mp\cfg-navitel\navitel-uk.cfg  --target-lang=uk --default-lang=uk  --defaultcountry="UA" -o maps\Navitel-%%A.mp extracts\%%A.osm
--- other/makebounds.bat
+++ other/makebounds.bat
@@ -1,5 +1,19 @@
 ..\executable\osmconvert ..\UA.osm.pbf --out-o5m >..\UA.o5m
+..\executable\osmfilter ..\UA.o5m --keep-nodes= --keep-ways-relations="boundary=administrative =postal_code postal_code= place=" --out-o5m >..\ua-boundaries.o5m
+..\executable\osmconvert ..\ua-boundaries.o5m --out-pbf -o=..\ua-boundaries.pbf
 
-..\executable\osmfilter ..\UA.o5m --keep-nodes= --keep-ways-relations="boundary=administrative =postal_code postal_code=" --out-o5m > ..\ua-boundaries.o5m
+..\executable\osmosis\bin\osmosis.bat ^
+ --read-pbf file=..\ua-boundaries.pbf outPipe.0=data1 ^
+ --read-pbf file=..\ua-boundaries.pbf outPipe.0=data2 ^
+ --tag-transform file=transform_places.xml inPipe.0=data1 outPipe.0=5 ^
+ --tag-filter accept-relations boundary=administrative,postal_code inPipe.0=5 outPipe.0=6 ^
+ --used-way inPipe.0=6 outPipe.0=7 ^
+ --tag-transform file=transform_places.xml inPipe.0=data2 outPipe.0=8.1 ^
+ --tag-filter reject-relations inPipe.0=8.1 outPipe.0=8 ^
+ --tag-filter accept-ways boundary=administrative,postal_code inPipe.0=8 outPipe.0=9 ^
+ --used-node inPipe.0=9 outPipe.0=10 ^
+ --used-node inPipe.0=7 outPipe.0=11 ^
+ --merge inPipe.0=10 inPipe.1=11 outPipe.0=12 ^
+ --write-pbf file=..\ua-boundaries.osm.pbf omitmetadata=true compress=deflate inPipe.0=12 
 
-java -cp ..\executable\mkgmap\mkgmap.jar uk.me.parabola.mkgmap.reader.osm.boundary.BoundaryPreprocessor ..\ua-boundaries.o5m ..\executable\mkgmap\bounds
+java -cp ..\executable\mkgmap\mkgmap.jar uk.me.parabola.mkgmap.reader.osm.boundary.BoundaryPreprocessor ..\ua-boundaries.osm.pbf ..\executable\mkgmap\bounds

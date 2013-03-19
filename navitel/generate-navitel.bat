14f27729c479a5f19e5a5ab6b1b0bb0c531fa1d5
Larry0ua <pekarskiy.valeriy@gmail.com>
2013-03-19 13:02:24 +0200

    Navitel generation - get rid of offset.exe - already in new getbound.pl

--- navitel/generate-navitel.bat
+++ navitel/generate-navitel.bat
@@ -2,9 +2,9 @@ for /F "tokens=1,2,3 delims=," %%A in (..\other\regions.csv) Do (
  rem %%A - region code
  rem %%B - relation ID
  rem %%C - UTF-8 region name
- if not exist extracts\%%A.poly ..\executable\getbound.pl --onering --noinner --api=op_ru -o extracts\%%A.poly %%B
- if not exist extracts\exp-%%A.poly ..\executable\offset\offset.exe extracts\%%A.poly extracts\exp-%%A.poly
- if not exist extracts\%%A.osm ..\executable\osmconvert ../UA.osm.pbf -B=extracts\exp-%%A.poly  -o=extracts\%%A.osm
+ if not exist extracts\%%A.poly ..\executable\getbound.pl --onering --noinner --api=op_ru --offset=0.2 -o extracts\%%A.poly %%B
+REM if not exist extracts\exp-%%A.poly ..\executable\offset\offset.exe extracts\%%A.poly extracts\exp-%%A.poly
+ if not exist extracts\%%A.osm ..\executable\osmconvert ../UA.osm.pbf -B=extracts\%%A.poly -o=extracts\%%A.osm
  if not exist maps\Navitel-%%A.mp ..\executable\osm2mp\osm2mp.pl --config=..\executable\osm2mp\cfg-navitel\navitel-uk.cfg  --target-lang=uk --default-lang=uk  --defaultcountry="UA" -o maps\Navitel-%%A.mp extracts\%%A.osm
 )
 if exist maps ConvertToNM2.vbs
